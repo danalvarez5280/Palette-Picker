799d59a83e18be72cc78f9ec00d18c6ddffe2315
Dan Alvarez <danalvarez5280@gmail.com>
2017-10-06 01:56:53 -0600

    made testing files, got them to sort of work, most will pass but i am getting an odd error and the before hook all is timing out the testing session.

---
+++ public/test/routes.spec.js
@@ -0,0 +1,182 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../../server');
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  //happy path
+  it('should return the home page with text', (done) => {
+   chai.request(server)
+   .get('/')
+   .end((error, response) => {
+     response.should.have.status(200);
+     response.should.be.html;
+     response.res.text.includes('Palette Picker');
+     done();
+   });
+ });
+
+  //sad path
+  it('should return a 404 for a route that does not exitst', (done) => {
+    chai.request(server)
+    .get('/bender')
+    .end((error, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
+});
+
+describe('API Routes', () => {
+
+  before(done => {
+    database.migrate.latest()
+    .then(() => done())
+    .catch(error => console.log(error))
+  });
+
+  beforeEach(done => {
+    database.seed.run()
+    .then(() => done())
+    .catch(error => console.log(error))
+  });
+
+  describe('GET /api/v1/projects', () => {
+    it('should return all the projects', (done) => {
+      chai.request(server)
+      .get('/api/v1/projects')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(3);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('JohnSnow');
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        done();
+      });
+    });
+
+    it('should return all of the palettes', (done) => {
+      chai.request(server)
+      .get('/api/v1/palettes')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(5);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('hello');
+        response.body[0].should.have.property('hex1');
+        response.body[0].hex1.should.equal('#C99307');
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(16);
+        done();
+      });
+    });
+  });
+
+  describe('POST /api/v1/projects', () => {
+    it('should save a project to the database', (done) => {
+      chai.request(server)
+      .post('/api/v1/projects')
+      .send({
+        name: 'LordSnow',
+      })
+      .end((error, response) => {
+        response.should.have.status(201);
+        response.body.should.be.a('object');
+        response.body.should.have.property('name');
+        response.body.name.should.equal('LordSnow');
+        chai.request(server)
+        .get('/api/v1/projects')
+        .end((error, response) => {
+          response.body.should.be.a('array');
+          response.body.length.should.equal(2);
+          done();
+        });
+      });
+    });
+
+    it('should not create a record with missing data', (done) => {
+      chai.request(server)
+      .post('/api/v1/projects')
+      .send({
+        notTheName: 1,
+      })
+      .end((error, response) => {
+        response.should.have.status(500);
+        // response.body.error.should.equal({ error });
+        done();
+      });
+    });
+
+    it('should save a palette to the database', (done) => {
+      chai.request(server)
+      .post('/api/v1/palettes')
+      .send({
+        name: 'A Nights Watch',
+        hex1: '#FFFFFF',
+        hex2: '#DDDDDD',
+        hex3: '#HHHHHH',
+        hex4: '#AAAAAA',
+        hex5: '#CCCCCC',
+        project_id: 3
+      })
+      .end((error, response) => {
+        response.should.have.status(201);
+        response.body.should.be.a('Array');
+        response.body.length.should.equal(1)
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('A Nights Watch');
+        response.body[0].should.have.property('hex1');
+        response.body[0].hex1.should.equal('#FFFFFF');
+        response.body[0].should.have.property('hex2');
+        response.body[0].hex2.should.equal('#DDDDDD');
+        response.body[0].should.have.property('hex3');
+        response.body[0].hex3.should.equal('#HHHHHH');
+        response.body[0].should.have.property('hex4');
+        response.body[0].hex4.should.equal('#AAAAAA');
+        response.body[0].should.have.property('hex5');
+        response.body[0].hex5.should.equal('#CCCCCC');
+        response.body[0].should.have.property('project_id');
+        response.body[0].project_id.should.equal(3);
+        chai.request(server)
+        .get('/api/v1/projects')
+        .end((error, response) => {
+          response.body.should.be.a('array');
+          response.body.length.should.equal(3);
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('DELETE /api/v1/palettes/:id', () => {
+   it('should delete a palette', (done) => {
+     chai.request(server)
+     .delete('/api/v1/palettes/17')
+     .end((error, response) => {
+       response.should.have.status(204);
+       done();
+     });
+   });
+
+   it('should return a 422 error if it could not find a palette', () => {
+     chai.request(server)
+     .delete('/api/v1/palettes/200')
+     .end((error, response) => {
+       response.should.have.status(422);
+       done();
+     })
+   })
+ });
+
+});
